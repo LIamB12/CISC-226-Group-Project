bbe5c7e4101763b278d6f2f5f5d7f17559735ef6
LIamB12 <liam.beenken@gmail.com>
2025-01-19 15:16:29 -0500

    update camera speed

--- Climber I hardly know her/Assets/Camera/CameraMovement.cs
+++ Climber I hardly know her/Assets/Camera/CameraMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float scrollSpeed = 1f; // Speed at which the camera moves upward
+    public float scrollSpeed = 3f; // Speed at which the camera moves upward
 
     void FixedUpdate()
     {
