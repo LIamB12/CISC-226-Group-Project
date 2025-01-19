f89313ce732654d090342784d7b5bf274bcb3a67
LIamB12 <liam.beenken@gmail.com>
2025-01-19 15:13:16 -0500

    add vertical scrolling and platforms

---
+++ Climber I hardly know her/Assets/Camera/CameraMovement.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float scrollSpeed = 1f; // Speed at which the camera moves upward
+
+    void FixedUpdate()
+    {
+        // Move the camera upward at a consistent speed
+        transform.position += Vector3.up * scrollSpeed * Time.fixedDeltaTime;
+    }
+}
