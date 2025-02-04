70c5aa91040d3dc38eb77d2a5b733d076c256afa
Finneganmac <119927082+Finneganmac@users.noreply.github.com>
2025-02-04 14:09:37 -0500

    Chuck WIP

---
+++ Climber I hardly know her/Assets/Player Classes/Chuck/Chuck.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Chuck : Player
+{
+    [Space]
+    [Header("Subclass Properties")]
+    [SerializeField] protected GameObject AbilityPrefab;
+
+    
+    protected override void UseAbility()
+    {
+        /*GameObject newProjectile = Instantiate(AbilityPrefab, (Vector2)this.transform.position + new Vector2(0.5f * facingDirection, 0), Quaternion.identity);
+        newProjectile.GetComponent<Fireball>().direction = facingDirection;
+        newProjectile.GetComponent<Fireball>().owner = this;
+        abilityCooldownTime = abilityCooldownMaxTime;*/
+        
+    }
+}
