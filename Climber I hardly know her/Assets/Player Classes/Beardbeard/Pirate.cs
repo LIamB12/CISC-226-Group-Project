e3c1c37609250f4f73e4a0fb1ad41e7db0329c32
Jacob McMullen <jacob.w.mcmullen@gmail.com>
2025-03-09 15:19:57 -0400

    changed pirate to state system
    makes managing his ability easier

--- Climber I hardly know her/Assets/Player Classes/Beardbeard/Pirate.cs
+++ Climber I hardly know her/Assets/Player Classes/Beardbeard/Pirate.cs
@@ -9,6 +9,15 @@ public class Pirate : Player
     [SerializeField] public int chargeTime;
     [SerializeField] public float chargePower;
 
+    // represents state of ability,
+    // charging = grounded, holding ability
+    // launched = ability released
+    // grounded = grounded, not holding ability
+    // notgrounded = not grounded, not holding ability
+    private enum abilityState{charging, launched, grounded, notGrounded}
+
+    private abilityState currentState = abilityState.notGrounded;
+
     [SerializeField] public GameObject anchorSwing;
 
     private new void Start()
@@ -22,7 +31,15 @@ public class Pirate : Player
     {
         base.FixedUpdate();
 
+        // controls the buildup of anchor 'charge'
+        if (isGrounded && Input.GetKey(key_Ability)){ // must be grounded to build 'charge'
+            chargeTime ++; // time spent holding button
+            chargePower = chargePower + 1 / math.log(chargeTime + 1); // charge power grows with diminishing returns
+        }
+
+
         if (isGrounded){ // must be grounded to build 'charge'
+            anchorSwing.SetActive(false);
 
             if (Input.GetKey(key_Ability)){
                 anchorSwing.SetActive(true);
@@ -30,10 +47,9 @@ public class Pirate : Player
                 chargePower = chargePower + 1 / math.log(chargeTime + 1); // charge power grows with diminishing returns
             }
             else if(chargeTime > 0){
-                isGrounded = false; // makes "J" launch better but doesnt fix it
-                anchorSwing.SetActive(false);
-                
-                rb.linearVelocity = new Vector2(facingDirection*chargePower*4, chargePower*2); // apply force
+                anchorSwing.SetActive(true);
+                clampVelocity = false;
+                rb.linearVelocity = new Vector2(facingDirection*chargePower*2, chargePower*2); // apply force
                 
                 //reset charge values
                 chargeTime = 0;
